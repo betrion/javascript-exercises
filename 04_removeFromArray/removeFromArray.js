5e59673a33a640cb0b84970ff10e6b5691379c19
betrion <forteee@gmail.com>
2022-04-09 01:18:04 +0200

    sum of integers between 2 numbers

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function (array, ...remArg) {
-  return array.splice();
+const removeFromArray = function (array, ...remArgs) {
+  remArgs.forEach((arg) => {
+    const index = array.indexOf(arg);
+    if (index > -1) {
+      array.splice(index, 1);
+    }
+  });
+  return array;
 };
 
+console.log(removeFromArray([1, 2, 3, 4], 1, 2, 4));
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  let sum = 0;
+  if (a < 0 || b < 0 || typeof a != "number" || typeof b != "number") {
+    return "ERROR";
+  } else {
+    let smaller = a < b ? a : b;
+    let bigger = a > b ? a : b;
+    for (let i = smaller; i <= bigger; i++) {
+      sum += i;
+    }
+  }
+  return sum;
 };
-
+console.log(sumAll(1, -4));
 // Do not edit below this line
 module.exports = sumAll;
